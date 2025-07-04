1234112ecdff8bb8cca268961f8091632c1a6eee
Ian Wu <zxc5131210@gmail.com>
2025-07-05 00:41:33 +0800

    feat : remove no use import

--- utils/util.py
+++ utils/util.py
@@ -1,8 +1,4 @@
-import os
 import time
-import yaml
-from typing import Optional
-from selenium.webdriver.remote.webdriver import WebDriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException, NoSuchElementException
