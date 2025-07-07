049f05f6c9eae897b671d757d2eea9d6328a36e2
Ian Wu <zxc5131210@gmail.com>
2025-07-07 22:22:33 +0800

    refactor : config_models

--- utils/config_models.py
+++ utils/config_models.py
@@ -20,16 +20,14 @@ class TwitchStarcraftPageLocators(DataClassJsonMixin):
 @dataclass
 class TestConfig(DataClassJsonMixin):
     url: str
-    search_term: str # 這裡的 search_term 需要保留，因為你的 config.yaml 裡有它
+    search_term: str
     screenshot_filename: Path
 
     def __post_init__(self):
-        # 確保 screenshot_filename 是 Path 物件
         self.screenshot_filename = Path(self.screenshot_filename)
         if self.screenshot_filename.suffix not in VALID_SCREENSHOT_EXTENSION:
             raise RuntimeError('Invalid screenshot file extension')
 
-# 定位器映射，也放在這裡，因為它直接與 dataclass 相關聯
 LOCATOR_MAPPING = {
     "twitch_home_page_locators": TwitchHomePageLocators,
     "twitch_starcraft_page_locators": TwitchStarcraftPageLocators,
